ac70fddebb2812427e2e9ae85754b594e63a9ad9
admin <admin>
2018-09-11 20:08:05 +0000

    Export: ok

---
+++ shared/IUCConfigs.lua
@@ -0,0 +1,11 @@
+local iucConf = {}
+
+-- Configurations for patient csv File
+iucConf.patientCsvPath = 'C:\\TempFolder\\CSV\\'
+iucConf.patientCsvFile = 'PatientData.txt'
+
+-- Configurations for patient DB file
+iucConf.patientDbPath = 'C:\\TempFolder\\DB\\'
+iucConf.patientDbFile = 'myDB.sqlite'
+
+return iucConf
